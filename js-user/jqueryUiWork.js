7753ab5f5baef6b6cde2bf5600caced39d573b4a
EPHANTUS <ephigach@gmail.com>
2015-12-04 20:30:54 +0300

    Progress bar added and open source stuff

--- js-user/jqueryUiWork.js
+++ js-user/jqueryUiWork.js
@@ -1,6 +1,51 @@
- $(document).ready(function() {
- 	//here we create an accordion
+$(document).ready(function()//wen jquery has loaded call this function
+{
+//here we create an accordion
+$( "#accordion" ).accordion();
+$( "#tabs" ).tabs();
 
- 	$("#accordion").accordion();
- 	$("#tabs").tabs();
- });
+
+// var listTemplate= '
+// 	<ul>
+// 			<li>
+// 			<h1>  <a href="#repo_link "> Repository title</a></h1>
+// 			<p>Description</p>
+// 			<span> Last updated:<time>12:00pm</time></span>
+
+// 			</li>
+// 		</ul>
+// 		';
+
+url = 'https://api.github.com/users/ephantus/repos';
+$.get(url, function(data)
+{
+	var listTemplate ='';
+		listTemplate += '<ul>';
+	data.forEach(function(repository)
+	{
+		
+		listTemplate += '<li>';
+		listTemplate += '<h1><a href="'+repository.html_url+'">'+repository.name+'</a></h1>';
+		listTemplate += '<p>'+repository.description+'</p>';
+		listTemplate += '<span> Last updated:<time>12:00pm</time></span>';
+		listTemplate += '</li>';
+		
+
+	});
+	listTemplate += '</ul>';
+
+
+$('#open-source-container').html(listTemplate);
+	
+});
+
+// $('#open-source-container').html(listTemplate);
+
+});
+
+
+
+
+
+
+	
